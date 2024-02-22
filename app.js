5500e916546cd9888f2f092db7c4126a03158b62
lexborelli <alexasborelli@gmail.com>
2024-02-22 18:58:04 -0500

    added new server code on app.js

--- app.js
+++ app.js
@@ -69,7 +69,6 @@ app.get('/',(req, res, next) => {
 
  //Turn on express server
 
- app.listen(3000, () => {
-    console.log('server listening on port 3000');
- });
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
